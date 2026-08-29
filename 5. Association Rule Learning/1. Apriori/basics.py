91d7de58b225629022eab0d37b2106e6bedc9f60
Akash-Choudaj <choudajakash03@gmail.com>
2026-08-30 01:38:27 +0530

    Implement Apriori model training with specified parameters for market basket analysis

--- 5. Association Rule Learning/1. Apriori/basics.py
+++ 5. Association Rule Learning/1. Apriori/basics.py
@@ -10,3 +10,13 @@ transaction = []
 for i in range(0, 7501):
     transaction.append([str(dataset.values[i, j]) for j in range(0, 20)])
 
+# Training the apriori model on dataset
+from apyori import apriori
+rules = apriori(
+    transactions=transaction,
+    min_support=0.03,
+    min_confidince=0.2,
+    min_lift=3,
+    min_lenght=2,
+    max_length=2
+)
