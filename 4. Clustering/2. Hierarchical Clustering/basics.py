20896168e9bafd1b6dc329a7f18ba851616ef71b
Akash-Choudaj <choudajakash03@gmail.com>
2026-08-29 23:46:57 +0530

    Fix variable name from 'X' to 'x' in cluster visualization and clean up import statement

--- 4. Clustering/2. Hierarchical Clustering/basics.py
+++ 4. Clustering/2. Hierarchical Clustering/basics.py
@@ -15,11 +15,8 @@ plt.ylabel('Euclidean Distances')
 plt.show()
 
 # Training Hierarchical model on dataset
-# Import Agglomerative Clustering algorithm
-from sklearn.cluster import AgglomerativeClustering
 
 from sklearn.cluster import AgglomerativeClustering
-
 # Create the Hierarchical Clustering model
 hc = AgglomerativeClustering(
     n_clusters=3,          # Number of clusters
@@ -31,11 +28,11 @@ hc = AgglomerativeClustering(
 y_hc = hc.fit_predict(x)
 
 # Visualising the clusters
-plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
-plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
-plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
-plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(x[y_hc == 0, 0], x[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(x[y_hc == 1, 0], x[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(x[y_hc == 2, 0], x[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(x[y_hc == 3, 0], x[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(x[y_hc == 4, 0], x[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
 plt.title('Clusters of customers')
 plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (1-100)')
