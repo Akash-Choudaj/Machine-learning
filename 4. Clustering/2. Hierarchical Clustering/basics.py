943c98c2ded63cee3890f6aecd4dc862ebf39f1e
Akash-Choudaj <choudajakash03@gmail.com>
2026-08-29 23:42:39 +0530

    Add Agglomerative Clustering model implementation for Hierarchical Clustering

--- 4. Clustering/2. Hierarchical Clustering/basics.py
+++ 4. Clustering/2. Hierarchical Clustering/basics.py
@@ -13,3 +13,17 @@ plt.title('Dendrogram')
 plt.xlabel('Customers')
 plt.ylabel('Euclidean Distances')
 plt.show()
+
+# Training Hierarchical model on dataset
+# Import Agglomerative Clustering algorithm
+from sklearn.cluster import AgglomerativeClustering
+
+# Create the Hierarchical Clustering model
+hc = AgglomerativeClustering(
+    n_clusters=3,          # Number of clusters we want
+    affinity='euclidean',  # Distance method used to measure similarity
+    linkage='ward'         # Method used to decide which clusters to merge
+)
+
+# Train the model and assign each data point to a cluster
+y_hc = hc.fit_predict(x)
