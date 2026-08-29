bd856454856828bd607bfa477fa1cd374ce248ec
Akash-Choudaj <choudajakash03@gmail.com>
2026-08-30 02:47:23 +0530

    Add visualization and DataFrame organization for Apriori results

--- 5. Association Rule Learning/1. Apriori/basics.py
+++ 5. Association Rule Learning/1. Apriori/basics.py
@@ -20,3 +20,25 @@ rules = apriori(
     min_lenght=2,
     max_length=2
 )
+
+# Visualising the results
+
+## Displaying the first results coming directly from the output of the apriori function
+results = list(rules)
+results
+
+## Putting the results well organised into a Pandas DataFrame
+def inspect(results):
+    lhs         = [tuple(result[2][0][0])[0] for result in results]
+    rhs         = [tuple(result[2][0][1])[0] for result in results]
+    supports    = [result[1] for result in results]
+    confidences = [result[2][0][2] for result in results]
+    lifts       = [result[2][0][3] for result in results]
+    return list(zip(lhs, rhs, supports, confidences, lifts))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
+
+## Displaying the results non sorted
+resultsinDataFrame
+
+## Displaying the results sorted by descending lifts
+resultsinDataFrame.nlargest(n = 10, columns = 'Lift')
