60087643a04c162ad21761d6b1c60ba0785eadd5
Akash-Choudaj <choudajakash03@gmail.com>
2026-08-29 23:44:56 +0530

    Add visualization for clusters in Agglomerative Clustering implementation

--- 4. Clustering/2. Hierarchical Clustering/basics.py
+++ 4. Clustering/2. Hierarchical Clustering/basics.py
@@ -18,12 +18,26 @@ plt.show()
 # Import Agglomerative Clustering algorithm
 from sklearn.cluster import AgglomerativeClustering
 
+from sklearn.cluster import AgglomerativeClustering
+
 # Create the Hierarchical Clustering model
 hc = AgglomerativeClustering(
-    n_clusters=3,          # Number of clusters we want
-    affinity='euclidean',  # Distance method used to measure similarity
-    linkage='ward'         # Method used to decide which clusters to merge
+    n_clusters=3,          # Number of clusters
+    metric='euclidean',    # Distance used to measure similarity
+    linkage='ward'         # Method used to merge clusters
 )
 
-# Train the model and assign each data point to a cluster
+# Fit the model and assign each data point to a cluster
 y_hc = hc.fit_predict(x)
+
+# Visualising the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
