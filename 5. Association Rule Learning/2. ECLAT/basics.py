b4df010d1e63dffd85fceff5ecb0dead709b59a9
Akash-Choudaj <choudajakash03@gmail.com>
2026-08-30 22:27:34 +0530

    Add ECLAT model training implementation in basics.py

--- 5. Association Rule Learning/2. ECLAT/basics.py
+++ 5. Association Rule Learning/2. ECLAT/basics.py
@@ -9,3 +9,7 @@ transactions = []
 for i in range(0, 7501):
   transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
 
+# Training the Eclat model on the dataset
+from apyori import apriori
+rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
+
