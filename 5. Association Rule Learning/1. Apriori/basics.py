72a9dfa233f627a3f333e1e53cffd06d111d54bd
Akash-Choudaj <choudajakash03@gmail.com>
2026-08-30 21:56:55 +0530

    Refactor data preprocessing and improve variable naming for clarity in Apriori implementation

--- 5. Association Rule Learning/1. Apriori/basics.py
+++ 5. Association Rule Learning/1. Apriori/basics.py
@@ -1,25 +1,17 @@
+# Importing the libraries
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import apyori as ap
+import pandas as pd
 
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header= None)
-
-# Data Preprocessing for dataset
-transaction = []
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
 for i in range(0, 7501):
-    transaction.append([str(dataset.values[i, j]) for j in range(0, 20)])
+  transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
 
-# Training the apriori model on dataset
+# Training the Apriori model on the dataset
 from apyori import apriori
-rules = apriori(
-    transactions=transaction,
-    min_support=0.03,
-    min_confidince=0.2,
-    min_lift=3,
-    min_lenght=2,
-    max_length=2
-)
+rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
 
 # Visualising the results
 
