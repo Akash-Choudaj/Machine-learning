10abfad22deff8f7670f474a1d52eefb677075e4
Akash-Choudaj <choudajakash03@gmail.com>
2026-08-31 00:26:49 +0530

    Add initial implementation of UCB algorithm for ad click optimization
    - Created a new Python file for the UCB algorithm.
    - Imported necessary libraries: numpy, pandas, and matplotlib.
    - Loaded the dataset containing click-through rates for 10 ads.

---
+++ 6. Reinforcement Learning/UCB.py
@@ -0,0 +1,9 @@
+# importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv') # dataset is about highest click rate for all 10 ads
+
+
+
